68e57b604e8a1ec119c437b94ca5f699d7f296d4
Kenneth F Cannon <kennethfcannon@gmail.com>
2018-11-07 18:35:23 -0500

    done

--- src/functions.test.js
+++ src/functions.test.js
@@ -6,12 +6,51 @@
 
 // ...
 
+// Define function with 2 arguments max(number1, number2)
+//
+
+function max(num1, num2) {
+  if (num1 > num2) {
+    return num1
+  } else {
+    return num2
+  }
+}
+
+// function max(number1, number2) {
+//   let num1 = number1
+//   let num2 = number2
+
+//   if (num1 > num2) {
+//     return num1
+//   } else {
+//     return num2
+//   }
+// }
+
+// t.is(max(1, 3), 3)
+// t.is(max(0, 3), 3)
+
 /**
  * Define a function maxOfThree() that takes three
  * numbers as arguments and returns the largest of them.
  */
 
 // ...
+
+function maxOfThree(number1, number2, number3) {
+  let num1 = number1
+  let num2 = number2
+  let num3 = number3
+
+  if (num1 > num2 && num1 > num3) {
+    return num1
+  } else if (num2 > num1) {
+    return num2
+  } else {
+    return num3
+  }
+}
 
 /*
  * Define a function sum() that takes two numbers as
@@ -20,12 +59,36 @@
 
 // ...
 
+// function sum(total) {
+//   total = num1 + num2
+// }
+// sum(num1, num2)
+
+function sum(num1, num2) {
+  return num1 + num2
+}
+
 /*
  * Define a function sumOfArray that calculates the sum of
  * all the numbers in an array.
  */
 
 // ...
+// let total = 0
+
+function sumOfArray(array) {
+  let total = 0
+  array.forEach(number => {
+    total += number
+  })
+  return total
+}
+
+// array.forEach(function sumOfArray(array) {
+//   array + array
+// })
+
+// return array
 
 /**
  * Write a function isVowel() that takes a character (i.e. a string of length 1)
@@ -34,14 +97,21 @@
 
 // ...
 
- /**
-  * Write a function rovarspraket() that will translate
-  * a text into a "rövarspråket". That is, double every
-  * consonant and place an occurrence of "o" in between.
-  *
-  * For example, rovarspraket("this is fun") should
-  * return the string "tothohisos isos fofunon".
-  */
+let isVowel = letter => {
+  let vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+  let answer = vowels.filter(vowel => {
+    return letter === vowel
+  })
+  return answer.length === 1
+}
+/**
+ * Write a function rovarspraket() that will translate
+ * a text into a "rövarspråket". That is, double every
+ * consonant and place an occurrence of "o" in between.
+ *
+ * For example, rovarspraket("this is fun") should
+ * return the string "tothohisos isos fofunon".
+ */
 
 // ...
 
@@ -54,12 +124,12 @@
 
 // ...
 
- /**
-  * Write a function findLongestWord() that takes an
-  * string returns the first, longest word in the array.
-  *
-  * i.e. findLongestWord("book dogs") should return "book"
-  */
+/**
+ * Write a function findLongestWord() that takes an
+ * string returns the first, longest word in the array.
+ *
+ * i.e. findLongestWord("book dogs") should return "book"
+ */
 
 // ...
 
@@ -71,7 +141,7 @@
 
 import test from 'ava'
 
-test('max()', (t) => {
+test('max()', t => {
   t.is(max(1, 3), 3)
   t.is(max(0, 3), 3)
   t.is(max(10, 3), 10)
@@ -80,7 +150,7 @@ test('max()', (t) => {
   t.true(isNaN(max('aaa', 'bbb')))
 })
 
-test('maxOfThree()', (t) => {
+test('maxOfThree()', t => {
   t.is(maxOfThree(1, 3, 2), 3)
   t.is(maxOfThree(0, 3, -1), 3)
   t.is(maxOfThree(10, 3, 50), 50)
@@ -89,19 +159,19 @@ test('maxOfThree()', (t) => {
   t.true(isNaN(maxOfThree('aaa', 'bbb', 'ccc')))
 })
 
-test('sum()', (t) => {
+test('sum()', t => {
   t.is(sum(8, 11), 19)
   t.is(sum(4, 100), 104)
 })
 
-test('sumOfArray()', (t) => {
+test('sumOfArray()', t => {
   t.is(sumOfArray([1, 2]), 3)
   t.is(sumOfArray([1, 2, 3]), 6)
   t.is(sumOfArray([10, 9, 8]), 27)
   t.is(sumOfArray([]), 0)
 })
 
-test('isVowel()', (t) => {
+test('isVowel()', t => {
   t.is(isVowel(0), false)
   t.is(isVowel('B'), false)
   t.is(isVowel('b'), false)
@@ -110,7 +180,7 @@ test('isVowel()', (t) => {
   t.is(isVowel('E'), true)
 })
 
-test('rovarspraket()', (t) => {
+test('rovarspraket()', t => {
   t.is(rovarspraket('a'), 'a')
   t.is(rovarspraket('b'), 'bob')
   t.is(rovarspraket('cat'), 'cocatot')
@@ -118,12 +188,12 @@ test('rovarspraket()', (t) => {
   t.is(rovarspraket(0), '0')
 })
 
-test('reverse()', (t) => {
+test('reverse()', t => {
   t.is(reverse('books'), 'skoob')
   t.is(reverse("we don't want no trouble"), "elbuort on tnaw t'nod ew")
 })
 
-test('findLongestWord()', (t) => {
+test('findLongestWord()', t => {
   t.is(findLongestWord('book dogs'), 'book')
   t.is(findLongestWord('life the universe and everything'), 'everything')
 })
